703e566c316171a37e68afb13a30b429001b9263
kevinjustal <kevin.justal@smood.ch>
2022-03-11 17:40:31 +0100

    [CONFIG] Adding the listing of db

--- server1/index.js
+++ server1/index.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const NodeCouchdb = require('node-couchdb');
+
+const couch = new NodeCouchdb(
+  {
+    host: 'couchdb',
+    protocol: 'http',
+    port: 5984,
+    auth: {
+      user: 'admin',
+      password: 'test'
+    }
+  }
+);
+
+couch.listDatabases().then(function(dbs){
+  console.log(dbs);
+});
 
 app.get('/', (req, res) => {
   res.send({
