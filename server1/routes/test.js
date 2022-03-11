c4bce5e4d36529d28c0597cc238bbd49e4457aab
justalk <justal.kevin@gmail.com>
2022-03-11 03:45:13 +0100

    [FEATURE] Adding some calls

--- server1/routes/test.js
+++ server1/routes/test.js
@@ -15,23 +15,6 @@ router.post('/create', async (request, response) => {
   response.status(200).send(rsl)
 })
 
-// Get all object inside the couchdb
-router.get('/all', async (request, response) => {
-  // include doc give you the real document
-  const rsl = await testDB.list({include_docs: true})
-  response.status(200).send(rsl)
-})
 
-// Get one object inside the couchdb
-router.get('/one/:id', async (request, response) => {
-  const rsl = await testDB.get(request.params.id)
-  response.status(200).send(rsl)
-})
-
-// Delete one object inside the couchdb
-router.delete('/d/:id/:rev', async (request, response) => {
-  const rsl = await testDB.destroy(request.params.id, request.params.rev)
-  response.status(200).send(rsl)
-})
 
 module.exports = router
