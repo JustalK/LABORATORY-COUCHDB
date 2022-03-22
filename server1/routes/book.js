213eec7215b4cd7bbfbc1dc6e6dac33216089e1e
justalk <justal.kevin@gmail.com>
2022-03-22 05:09:35 +0100

    [FEATURE] Adding the func function and starting to graps the map/emit

--- server1/routes/book.js
+++ server1/routes/book.js
@@ -23,6 +23,10 @@ router.get('/all/selected', async (request, response) => {
   response.status(200).send(body)
 })
 
+const func = function(doc) {
+  emit(doc.title, doc._id);
+}
+
 // Create an object inside the couchdb
 router.post('/create', async (request, response) => {
   booksDB.insert(
@@ -33,6 +37,16 @@ router.post('/create', async (request, response) => {
           map: function(doc) {
             emit([doc.title], doc._id);
           }
+        },
+        all_books2:
+        {
+          map: function(doc) {
+            emit(doc.title, doc._id);
+          }
+        },
+        all_books3:
+        {
+          map: func.toString()
         },
         book_restriction:
         {
@@ -52,17 +66,27 @@ router.post('/create', async (request, response) => {
           reduce: '_count'
         }
       }
-    }, '_design/books_all4');
+    }, '_design/books_all6');
   response.status(200).send(true)
 })
 
+router.get('/all/selected2', async (request, response) => {
+  const body = await booksDB.view('books_all6', 'all_books2')
+  response.status(200).send(body)
+})
+
+router.get('/all/selected3', async (request, response) => {
+  const body = await booksDB.view('books_all6', 'all_books3')
+  response.status(200).send(body)
+})
+
 router.get('/all/restricted', async (request, response) => {
-  const body = await booksDB.view('books_all', 'book_restriction')
+  const body = await booksDB.view('books_all6', 'book_restriction')
   response.status(200).send(body)
 })
 
 router.get('/all/count', async (request, response) => {
-  const body = await booksDB.view('books_all4', 'book_count_js')
+  const body = await booksDB.view('books_all6', 'book_count_js')
   response.status(200).send(body)
 })
 
